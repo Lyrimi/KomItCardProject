c39fb119a125f5319026c055374830a9b97246b3
Lyrimi <lyrimisin@gmail.com>
2026-01-22 19:48:50 +0100

    Im tired

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,22 +1,135 @@
 using UnityEngine;
+using System.Collections.Generic;
 using Element = Globals.ElementLookup;
+using Unity.VisualScripting;
+using System;
+using System.Threading.Tasks;
+using TMPro;
+using System.IO.IsolatedStorage;
 
 public class CombatManager : MonoBehaviour
 {
     [SerializeField] PlayerHandler Player;
     [SerializeField] EnemyHandler Enemy;
 
+    [SerializeField] int MaxPlayerHealth;
+    [SerializeField] int MaxEnemyHealth;
+
+    [SerializeField] TextMeshProUGUI PlayerStatusText;
+    [SerializeField] TextMeshProUGUI EnemyStatusText;
+
+    [Header("Don't touch debug stuff")]
+
+
+    [SerializeField] int PlayerHealth;
+    [SerializeField] int EnemyHealth;
+    [SerializeField] string[] Status;
+    struct Ids
+    {
+        public const int Player = 0;
+        public const int Enemy = 1;
+    }
+    Dictionary<string, Element> StringToElement;
+    Dictionary<string, int> EffectDamageLookUp;
+
     Element[] Elements;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (Status.Length < 2)
+        {
+            Status = new string[2];
+        }
         Elements = Globals.ElementLookups;
+        StringToElement = Globals.StringToElement;
+        EffectDamageLookUp = Globals.EffectDamageLookUp;
+
+        PlayerHealth = MaxPlayerHealth;
+        EnemyHealth = MaxEnemyHealth;
+        print(GetEffectForTarget(1, "Water"));
+        ResolveDamage();
+        UpdateStatus();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    string GetEffectForTarget(int TargetId, string Element)
+    {
+        string CurrentStatus = Status[TargetId];
+        Element elm = StringToElement[Element];
+        if (!string.IsNullOrEmpty(CurrentStatus) && elm.ReactionDict.ContainsKey(CurrentStatus))
+        {
+            return elm.ReactionDict[CurrentStatus];
+        }
+        return elm.DefaultEffect;
+    }
+
+    async Task AttackTarget(int TargetId, string Element)
+    {
+        Status[TargetId] = GetEffectForTarget(TargetId, Element);
+        await Task.Delay(1000);
+    }
+
+    async Task ResolveDamage()
+    {
+        while (IsReflected() != -1)
+        {
+            int Reflect = IsReflected();
+            if (Reflect == Ids.Enemy)
+            {
+                await AttackTarget(Ids.Player, "Rock");
+                Status[Ids.Enemy] = "";
+            }
+            if (Reflect == Ids.Player)
+            {
+                await AttackTarget(Ids.Enemy, "Rock");
+                Status[Ids.Player] = "";
+            }
+        }
+
+        for (int i = 0; i < Status.Length; i++)
+        {
+            string status = Status[i];
+            if (!(!string.IsNullOrEmpty(status) && EffectDamageLookUp.ContainsKey(status)))
+            {
+                continue;
+            }
+            if (i == Ids.Player)
+            {
+                PlayerHealth -= EffectDamageLookUp[status];
+            }
+            else
+            {
+                EnemyHealth -= EffectDamageLookUp[status];
+            }
+        }
+    }
+
+    int IsReflected()
+    {
+        for (int i = 0; i < Status.Length; i++)
+        {
+            if (Status[i] == "Reflect")
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void ApplyDefense()
+    {
+
+    }
+    
+    void UpdateStatus()
+    {
+        PlayerStatusText.text = Status[Ids.Player];
+        EnemyStatusText.text = Status[Ids.Enemy];
     }
 }
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -8,9 +8,14 @@ public class Globals : MonoBehaviour
     [SerializeField] private Sprite[] Sprites;
     [SerializeField] private string[] Keys;
     static public Dictionary<string, Sprite> SpriteDictionary;
+    static public Dictionary<string, ElementLookup> StringToElement;
     [Header("Elements")]
     public List<Element> Elements;
-    
+
+    public string[] EffectDmgKeys;
+    public int[] EffectDmgValues;
+    static public Dictionary<string, int> EffectDamageLookUp;
+
     [System.Serializable]
     public struct ElementReaction
     {
@@ -21,14 +26,17 @@ public class Globals : MonoBehaviour
     [System.Serializable]
     public class Element
     {
+        public string Name;
         public string DefaultEffect;
         public List<ElementReaction> Reactions;
     }
 
     public class ElementLookup
     {
+        public string Name;
         public string DefaultEffect;
         public Dictionary<string, string> ReactionDict;
+        
     }
 
     public static ElementLookup[] ElementLookups;
@@ -38,8 +46,9 @@ public class Globals : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        BuildDictionary();
+        BuildDictionarys();
         BuildElements();
+        BuildStringToElement();
     }
 
 
@@ -51,16 +60,26 @@ public class Globals : MonoBehaviour
         {
             ElementLookups[i] = new ElementLookup();
             ElementLookups[i].DefaultEffect = Elements[i].DefaultEffect;
+            ElementLookups[i].Name = Elements[i].Name;
             ElementLookups[i].ReactionDict = new Dictionary<string, string>();
             foreach (ElementReaction reaction in Elements[i].Reactions)
             {
                 ElementLookups[i].ReactionDict[reaction.ReactionKey] = reaction.EffectValue;
             }
         }
-        print(ElementLookups[0].ReactionDict["Burn"]);
+    }
+
+    void BuildStringToElement()
+    {
+         StringToElement = new Dictionary<string, ElementLookup>();
+
+        foreach (ElementLookup elementLookup in ElementLookups)
+        {
+            StringToElement.Add(elementLookup.Name, elementLookup);
+        }
     }
     
-    private void BuildDictionary()
+    private void BuildDictionarys()
     {
         SpriteDictionary = new Dictionary<string, Sprite>();
 
@@ -87,5 +106,32 @@ public class Globals : MonoBehaviour
 
             SpriteDictionary.Add(Keys[i], Sprites[i]);
         }
+
+        EffectDamageLookUp = new Dictionary<string, int>();
+
+         // Safety check
+        if (EffectDmgKeys.Length != EffectDmgValues.Length)
+        {
+            Debug.LogError("Keys and Sprites arrays must have the same length!");
+            return;
+        }
+
+        for (int i = 0; i < EffectDmgKeys.Length; i++)
+        {
+            if (string.IsNullOrEmpty(Keys[i]))
+            {
+                Debug.LogWarning($"Key at index {i} is null or empty, skipping.");
+                continue;
+            }
+
+            if (EffectDamageLookUp.ContainsKey(Keys[i]))
+            {
+                Debug.LogWarning($"Duplicate key '{Keys[i]}' found, skipping.");
+                continue;
+            }
+
+            EffectDamageLookUp.Add(EffectDmgKeys[i], EffectDmgValues[i]);
+        }
+
     }
 }
